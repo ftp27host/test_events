10645e4e20c5c08fc98ddb2e5d90a6db35b7ea6f
A. Cherepanov <ftp27host@gmail.com>
2015-04-28 15:13:38 +0300

    Add urls fix for statics and media

--- eventer/urls.py
+++ eventer/urls.py
@@ -1,7 +1,13 @@
 from django.conf.urls import include, url
 from django.contrib import admin
+from django.conf.urls.static import static
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
+import settings
 
 urlpatterns = [
     url(r'^admin/', include(admin.site.urls)),
     url(r'^api/', include('events.urls', namespace='events')),
 ]
+
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+urlpatterns += staticfiles_urlpatterns()
--- events/urls.py
+++ events/urls.py
@@ -2,6 +2,7 @@ from django.conf.urls import include, url
 from . import views
 
 urlpatterns = [
-    url(r'^categories/$', views.categoryList, name='categoryList'),
+    url(r'^categories/$', views.CategoryList, name='categoryList'),
     url(r'^category/(?P<category_id>[0-9]+)/$', views.category, name='category'),
+    url(r'^now/$', views.categoriesNow, name='categoriesNow'),
 ]
